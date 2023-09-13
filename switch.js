4ca18a6e80bd28223b369420fc83872f1f389c4d
Daniel Giraldo <144749559+JoseGP39@users.noreply.github.com>
2023-09-13 10:55:33 -0500

    Update switch.js
    Ejemplo de ciclo SWITCH, donde validamos los días de la semana, se le solicita al usuario ingresar "día de la semana" y si este ingrese una palabra que no corresponda a día, nos devuelve un mensaje diciendo "día no valido"

--- switch.js
+++ switch.js
@@ -1,1 +1,25 @@
+//ejercicio condicional switch para compaarar varios datos
 
+//declaramos variable para capturar dato de entrada
+var dia = prompt("Ingresa el dia de la semana");
+
+switch (dia) {
+    case "lunes":
+        alert("lunes");
+        break;
+    case "martes":
+        alert("martes");
+        break;
+    case "miercoles":
+        alert("miercoles");
+        break;
+    case "jueves":
+        alert("jueves");
+        break;
+    case "viernes":
+        alert("viernes");
+        break;
+        default: //con esta palabra reservada si no ingresamos un dia valido, nos muestra un mensaje 
+        alert("dia no valido");
+
+}
